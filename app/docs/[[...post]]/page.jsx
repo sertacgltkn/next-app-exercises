a7f382f0f18215e25c7a7624ea7fd038778bf32f
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-06 12:31:57 +0300

    link attribute ex.

--- app/docs/[[...post]]/page.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function PostPage({ params }) {
-    return <div>post page {params.post}</div>;
-    }
-    
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
- 
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      hello world
+      <br />
+      <Link // same "a" tag
+      prefetch={false} //next.js prefetches the page by default, if you don't want it to do it, set it to false
+      target="_blank"
+      className="btn"
+      href={
+        {
+          pathname: "/docs/post",
+          query: { name: "Sertaç",   // url query
+                   surname: "Gltkn" },
+                   
+        }
+      }>Info page</Link>
     </div>
-  )
+  );
 }
