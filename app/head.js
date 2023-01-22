3974925c312f8d61d5483e816a4b7e03c3c45b56
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-22 12:59:34 +0300

    next/font comp. ex's

--- app/head.js
+++ app/head.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { getData } from "./page";
+//import { getData } from "./page";
 
-async function Head() {
+ function Head() {
 
-    const data = await getData();
-    console.log(data);
-    return <h1>{data.title}</h1>
+
+    
+    // const data = await getData();
+    // console.log(data);
+    // return <h1>{data.title}</h1>
     }
 
 export default Head;
--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,45 @@
 import React from "react";
+import { Inter, Open_Sans } from "@next/font/google";
 
-export const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-    cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
-    next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
-  });
-  return res.json;
-};
-export const getData2 = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/2", {
-    cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
-    next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
-  });
-  return res.json;
-};
-export const getData3 = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/3", {
-    cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
-    next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
-  });
-  return res.json;
-};
+const interFont = Inter({ subsets: ["latin"] });
+const openSansFont = Open_Sans({ subsets: ["latin"] });
 
-async function Home() {
-  const [getData, getData2, getData3] = await Promise.all([
-    getData(),
-    getData2(),
-    getData3(),
-  ])
+// export const getData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//     cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
+//     next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
+//   });
+//   return res.json;
+// };
+// export const getData2 = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/2", {
+//     cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
+//     next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
+//   });
+//   return res.json;
+// };
+// export const getData3 = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/3", {
+//     cache: "no-store", // In Next.js 13, the "no-store" feature prevents the page from being cached by the browser.
+//     next: { revalidate: 10 }, // so that the page can revalidate at any time we want.
+//   });
+//   return res.json;
+// };
 
-  console.log(data); 
-  return <h1>Data Fetching : {data.title}</h1>;
+function Home() {
+  // const [getData, getData2, getData3] = await Promise.all([
+  //   getData(),
+  //   getData2(),
+  //   getData3(),
+  // ])
+
+  // console.log(data);
+  return (
+    <div>
+      <h2 className={openSansFont.className}>what's up</h2>
+      <h1 className={interFont.className}>Hello world</h1>
+    </div>
+  );
 }
 
 export default Home;
